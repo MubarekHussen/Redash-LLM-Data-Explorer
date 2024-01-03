f408de0e68eb73d89e722151302a7cbfd8651acd
MubarekHussen <mubahussen2014@gmail.com>
2024-01-03 21:25:19 +0300

    feat: adding a script to prepare and parse data to database

---
+++ scripts/parse_data.py
@@ -0,0 +1,30 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+from urllib.parse import quote_plus
+
+root_dir = './youtube-data'
+
+# Create an empty dictionary to store dataframes
+dfs = {}
+
+encoded_password = quote_plus("Muba@sql14")
+engine = create_engine(f'postgresql://postgres:{encoded_password}@localhost:5432/youtube_data')
+
+for dirpath, dirnames, filenames in os.walk(root_dir):
+    for filename in filenames:
+        if filename.endswith('.csv') and filename != 'Totals.csv':
+            df_name = f"{os.path.basename(dirpath)}_{os.path.splitext(filename)[0]}"
+            df_name = df_name.replace(' ', '_').lower()
+            df = pd.read_csv(os.path.join(dirpath, filename))
+            dfs[df_name] = df
+
+Total_Views_by_Day = pd.read_csv('./youtube-data/Cities/Totals.csv')
+dfs['total_views_by_day'] = Total_Views_by_Day
+
+for df_name, df in dfs.items():
+    print(df_name)
+    print(df.head())
+
+for df_name, df in dfs.items():
+    df.to_sql(df_name, engine, if_exists='replace')
