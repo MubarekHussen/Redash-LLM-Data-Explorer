f1af3ff4a351fe6f413357cc1cc4dd5ce41b0018
Milliarde2 <rnmnat@gmail.com>
2024-01-04 23:25:21 +0300

    Commit for chat-gpt plugin

--- redash/handlers/api.py
+++ redash/handlers/api.py
@@ -1,3 +1,6 @@
+from redash.handlers.chat import (
+    ChatResource
+)
 from flask import make_response
 from flask_restful import Api
 from werkzeug.wrappers import Response
@@ -103,6 +106,7 @@ class ApiExt(Api):
 
 
 api = ApiExt()
+api.add_org_resource(ChatResource, "/api/chat", endpoint="chat")
 
 
 @api.representation("application/json")
